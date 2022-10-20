c32239098460db3d18bebc621afbf79f7cbc6b92
Jashwanthkanderi <85885830+Jashwanthkanderi@users.noreply.github.com>
2022-10-20 23:07:05 +0530

    Add files via upload

---
+++ Sample.js
@@ -0,0 +1,15 @@
+const backpack ={
+    name: "Everyday Backpack",
+    volume: 30,
+    color: "grey",
+    pocketNum: 15,
+    strapLength: {
+        left: 26,
+        right: 26,
+    },
+    lidOpen: false,
+    toggleLid: function(lidStatus) {
+        this.lidOpen = lidStatus;
+    }   
+}
+console.log(backpack);
